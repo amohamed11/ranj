d89849a5a6333674e109e695b90e1c4e2943d6fc
Anas Mohamed <14171441+amohamed11@users.noreply.github.com>
2022-01-22 00:51:19 -0700

    feat: piece background shading done

--- game/display.go
+++ game/display.go
@@ -4,49 +4,48 @@ import (
 	"fmt"
 )
 
-func getPieceUnicode(pieceNumber int, player int) string {
-	switch player {
-	case WHITE:
-		switch pieceNumber {
-		case ROOK:
-			return "♖"
-		case KNIGHT:
-			return "♘"
-		case BISHOP:
-			return "♗"
-		case KING:
-			return "♔"
-		case QUEEN:
-			return "♕"
-		case PAWN:
-			return "♙"
-		}
-	case BLACK:
-		switch pieceNumber {
-		case ROOK:
-			return "♜"
-		case KNIGHT:
-			return "♞"
-		case BISHOP:
-			return "♝"
-		case KING:
-			return "♚"
-		case QUEEN:
-			return "♛"
-		case PAWN:
-			return "♟︎"
-		}
+func whitePieceFg(s string) string {
+	return "\033[0;107m" + s + "\033[0m"
+}
 
+func blackPieceFg(s string) string {
+	return "\033[37;100m" + s + "\033[0m"
+}
+
+func getPieceUnicode(pieceNumber int, background int) string {
+	piece := "-"
+
+	switch pieceNumber {
+	case ROOK:
+		piece = "♜"
+	case KNIGHT:
+		piece = "♞"
+	case BISHOP:
+		piece = "♝"
+	case KING:
+		piece = "♚"
+	case QUEEN:
+		piece = "♛"
+	case PAWN:
+		piece = "♟︎"
 	}
 
-	return "-"
+	if background == BLACK {
+		return blackPieceFg(piece)
+	}
+
+	return whitePieceFg(piece)
 }
 
 func (b *Board) RenderBoard() {
 	for row := 0; row < len(b.cells); row++ {
 		for col := 0; col < len(b.cells[row]); col++ {
+			bg := WHITE
+			if (row+col)%2 != 0 { // if cell is even numbered
+				bg = BLACK
+			}
 			occupant := b.cells[row][col].occupant
-			fmt.Print(getPieceUnicode(occupant.pieceNum, occupant.owner))
+			fmt.Print(getPieceUnicode(occupant.pieceNum, bg))
 		}
 		fmt.Println()
 	}
